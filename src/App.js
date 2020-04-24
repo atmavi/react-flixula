bd6e5da497d23245e50a765badfe2b3de2b3c3f3
atmavi <atmavi0001@gmail.com>
2020-04-24 18:46:26 +0800

    Added carousel component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/nav";
 import Movies from "./components/movies";
+import Carousel from "./components/carousel";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Carousel />
       <Movies />
     </div>
   );
