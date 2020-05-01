a45dca0a962c156b904bdbd02a359fe03429fda1
atmavi <atmavi0001@gmail.com>
2020-05-01 18:31:18 +0800

    Initailized gallery component

---
+++ src/components/gallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gallery = () => {
+  return (
+    <div className="gallery">
+      <div className="row-full">1/1</div>
+      <div className="row-half">1/2</div>
+      <div className="row-third">1/3</div>
+    </div>
+  );
+};
+
+export default Gallery;
